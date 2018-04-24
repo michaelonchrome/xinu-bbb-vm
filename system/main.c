675ad828e2a8dbbcb567885ce8a5f102e5a8dcec
xinu <xinu@osboxes>
2018-04-23 19:27:09 -0700

    xinu for vm

---
+++ system/main.c
@@ -0,0 +1,25 @@
+/*  main.c  - main */
+
+#include <xinu.h>
+sid32 glbsem;
+process	main(void)
+{
+
+	/* Run the Xinu shell */
+
+    glbsem = semcreate(0);
+
+	recvclr();
+	resume(create(shell, 8192, 50, "shell", 1, CONSOLE));
+
+	/* Wait for shell to exit and recreate it */
+
+	while (TRUE) {
+		receive();
+		sleepms(200);
+		kprintf("\n\nMain process recreating shell\n\n");
+		resume(create(shell, 4096, 20, "shell", 1, CONSOLE));
+	}
+	return OK;
+    
+}
